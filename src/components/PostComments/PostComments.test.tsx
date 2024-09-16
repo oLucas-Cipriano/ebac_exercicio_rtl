2d7ccc761bc5bf4d4f7b76ef766405175d62da3e
lucas <lucas.e.a.cipriano@gmail.com>
2024-09-16 18:01:30 -0300

    Criando testes para adição de dois comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,20 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    test('teste para renderização de 2 comentarios', ()=>{
+        render(<PostComment/>);
+
+        const botao = screen.getByTestId('comentar');
+        const textArea = screen.getByTestId('textarea');
+
+        fireEvent.change(textArea, { target: { value: 'teste' } });
+        fireEvent.click(botao);
+
+        fireEvent.change(textArea, { target: { value: 'teste2' } });
+        fireEvent.click(botao);
+
+        expect(screen.getByText('teste')).toBeInTheDocument();
+        expect(screen.getByText('teste2')).toBeInTheDocument();
+        
+    })
 });
